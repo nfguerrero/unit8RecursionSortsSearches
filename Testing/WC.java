b6b6ef0d24cd856bd76b2a512e9ec58675fe5683
Nicolas Guerrero <nicg987@gmail.com>
2015-03-10 15:11:48 -0500

    WC stuff 2

--- Testing/WC.java
+++ Testing/WC.java
@@ -3,9 +3,10 @@ import java.io.File;
 
 public class WC
 {
-    public static void main(String[] blargs)
+    public static void main(String[] blargs) throws java.io.FileNotFoundException
     {
         Scanner nameScan = new Scanner(System.in);
+        System.out.print("File Name: ");
         File file = new File(nameScan.next());
         Scanner scan = new Scanner(file);
         
@@ -13,17 +14,32 @@ public class WC
         int words = 0;
         int lines = 0;
         
+        scan.useDelimiter("");
         while (scan.hasNext())
         {
-            if (scan.hasNextChar())
-            {
-                chars++;
-            }
-            else if (scan.hasNext())
-            {
-                
-            }
-            
+            String character = scan.next();
+            chars++;  
         }
+        scan = new Scanner(file);
+        System.out.println(chars);        
+        
+        scan.useDelimiter(" ");
+        while (scan.hasNext())
+        {
+            String word = scan.next();
+            words++;
+        }
+        scan = new Scanner(file);
+        System.out.println(words);
+        
+        scan.useDelimiter("\n");
+        while (scan.hasNext())
+        {
+            String line = scan.next();
+            lines++;
+        }
+        System.out.println(lines);
+        
+        scan.close();
     }
 }
