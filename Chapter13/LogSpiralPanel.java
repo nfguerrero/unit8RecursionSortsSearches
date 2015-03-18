feb7d824f30871794c314886e699e50b220b6f46
Nicolas Guerrero <nicg987@gmail.com>
2015-03-18 15:09:41 -0500

    spiral stuff

--- Chapter13/LogSpiralPanel.java
+++ Chapter13/LogSpiralPanel.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.Arc2D;
 import javax.swing.JPanel;
@@ -9,6 +10,14 @@ public class LogSpiralPanel extends JPanel
 
    public void paintComponent(Graphics g)
    {
+      Graphics2D g2 = (Graphics2D) g;
+      
+      double side = this.getHeight();
+      double x = 0;
+      double y = 0;
+      double angle = 0;
+      
+      this.recursiveDraw(g2, x, y, side, angle);
       /* Your code goes here.
          1. Compute the dimensions of the goldenRectangle (you can use getHeight() 
             to obtain the side size)
@@ -27,10 +36,67 @@ public class LogSpiralPanel extends JPanel
       golden rectangle is located. For the outermost golden rectangle, 
       the angle is 90.
    */
-   private void recursiveDraw(Graphics g, double x, double y, double side, int angle)
+   private void recursiveDraw(Graphics2D g2, double x, double y, double side, int angle)
    {
-      . . .
+      // Recursion ending condition: when the side is very small
+      if (side < 1)
+      {
+      }
+      else
+      {
+          recursiveDraw(g2, x, y, side, angle);
+      }
+
+      // Draw the current square and arc
+
+      /* Continue drawing the spiral recursively: calculate the new side 
+         size, calculate the new (x, y) coordinates and the new angle. Then, 
+         call "recursiveDraw" again. */
    }
    
-   . . .
+   /**
+      Draws the arc of the current iteration.
+      @param x The x-coordinate of the square's upper-left corner  
+      @param y The y-coordinate of the square's upper-left corner
+      @param side The size of the side of the square (or the arc's radius)
+      @param angle The angle (0, 90, 180 or 270) where the top of the 
+      current golden rectangle is located. For the outermost golden 
+      rectangle, the angle is 90.
+   */
+   private void drawArc(Graphics2D g2, double x, double y, double side, int angle)
+   {
+      double auxX = x;
+      double auxY = y;
+      if (angle == 0 || angle == 270 )
+      {
+         auxX = x - side;
+      }
+      if (angle == 270 || angle == 180)
+      {
+         auxY = y - side;
+      }
+      g2.drawArc((int) auxX, (int) auxY, (int) side * 2, (int) side * 2, angle, 90);
+   }  
+   
+   private double calculateNewX(double x, double angle, double side, double newSide)
+   {
+      if (angle == 0)
+         x = x + side - newSide;
+      else if (angle == 90)
+         x = x + side;
+      else if (angle == 270)
+         x = x - newSide;
+      return x;
+   }
+
+   private double calculateNewY(double y, double angle, double side, double newSide)
+   {
+      if (angle == 0)
+         y = y + side;
+      else if (angle == 180)
+         y = y - newSide;
+      else if (angle == 270)
+         y = y + side - newSide;
+      return y;
+   }
 }
